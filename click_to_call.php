24f367828fdb124481fdf9df16aca6e90816cfc1
Mike Jakobsen <mike@jakobsen.dk>
2016-12-19 14:45:33 +0100

    :art: Adding basic functionality

--- click_to_call.php
+++ click_to_call.php
@@ -1,11 +1,31 @@
 <?php
 
 /*
-	Plugin Name: Click To Call
-	Plugin URI: http://mikejakobsen.com
-	Description:
-	Author: Hoeks
-	Version: 0.0.1
-	Author URI: http://mikejakobsen.com
-	Text Domain: ....
+  Plugin Name: Click To Call
+  Plugin URI: http://mikejakobsen.com
+  Description:
+  Author: Hoeks
+  Version: 0.0.1
+  Author URI: http://mikejakobsen.com
+  Text Domain: ....
  */
+
+function if_mobile($atts, $content = null) {
+  wp_enqueue_script('ifmobile');
+  ob_start();
+  extract(shortcode_atts(array(
+    'device' => 'any',
+  ), $atts));
+?>
+
+  <script>
+  // Call Script
+  </script>
+
+/*
+  - Absolute positioned element - Maybe add overlay.
+ */
+
+<div class="if_mobile">
+    <?php echo $content; ?>
+</div>
