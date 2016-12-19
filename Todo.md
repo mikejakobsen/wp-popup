02910e875b043b6fefe14d8d3d4bacdd48233615
Mike Jakobsen <mike@jakobsen.dk>
2016-12-19 14:32:56 +0100

    :lipstick: Toggle Class on mobile
    Only supporting Android and iOS for now.

--- Todo.md
+++ Todo.md
@@ -7,6 +7,10 @@
 
 The template has to be a part of the *.php*, wordpress can't interact with *.js*.
 
+JQuery - Browser detect.
+
+http://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device-in-jquery
+
 ### Feature request
 
 - [ ] JS: Show after.. 5, 10 sec?
--- if_mobile.js
+++ if_mobile.js
@@ -1,4 +1,25 @@
 (function($){
-  let ifMobile = DoStuff;
-
+  let ifMobile = function (device, mobile) {
+    device.Android = mobile.match(/Android/i);
+    device.iOS = mobile.match(/iPhone|iPad|iPod/i);
+    device.any = device.Android || device.iOS;
+    device.selector = {
+      hide: '.on-desktop',
+      show: '.on-mobile'
+    };
+    device.show = function(){
+      $(device.selector.hide).hide();
+      $(device.selector.show).show();
+    };
+    device.hide = function(){
+      $(device.selector.show).hide();
+      $(device.selector.hide).show();
+    };
+    device.toggle = function(clause){
+      clause = typeof clause !== 'boolean' ? device.any : clause;
+      if (clause) device.show();
+      else device.hide();
+    };
+    return device;
+  }({}, navigator.userAgent);
 })(jQuery);
